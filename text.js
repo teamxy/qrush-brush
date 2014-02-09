a553d0e3d3aa7d1d75179a381a916563a7603a0d
shi-314 <zn31415926535@gmail.com>
2014-02-09 23:16:43 +0100

    Improve doge - such commit wow

--- text.js
+++ text.js
@@ -1,3 +1,17 @@
-function onClick(x,y){
-  text(x,y, 30,"wow, such font", "Comic Sans MS", 0xFF008800);
+var t = ["wow", "such font", "many brush", "so qt", "wow"];
+var c = [0xFFEE0000, 0xFF00FFFF, 0xFF33FF33, 0xFFFFFF00, 0xFFFF00FF];
+
+var ti = 0;
+var ci = 0;
+
+function onRelease(x,y){
+  var str = t[ti];
+  var col = c[ci];
+
+  text(x, y, Math.random() * 24 + 14, str, "Comic Sans MS", col);
+
+  ti = (ti + 1) % t.length;
+  ci = (ci + 1) % c.length;
+
+  log(str);
 }
