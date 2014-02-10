7489332d0e939a11136160208a73e9e3f58eacb7
shi-314 <zn31415926535@gmail.com>
2014-02-10 14:54:40 +0100

    Add more magic

--- image.js
+++ image.js
@@ -1,3 +1,3 @@
 function onClick(x,y){
-  image(x,y,600,400, "./doge.jpg");
+  image(x,y,600,400, "~/.qrush/doge.jpg");
 }
--- magic.js
+++ magic.js
@@ -2,6 +2,7 @@
  * From: http://perfectionkills.com/exploring-canvas-drawing-techniques/
  */
 var isDrawing, points = [ ];
+var color = 0x15333333;
 
 var onClick = function(x, y) {
   isDrawing = true;
@@ -14,12 +15,12 @@ var onDrag = function(x, y) {
   points.push({ x: x, y: y });
 
   for (var i = 1; i < points.length; i++) {
-    line(points[i-1].x, points[i-1].y, points[i].x, points[i].y, 0xFF000000);
+    line(points[i-1].x, points[i-1].y, points[i].x, points[i].y, color);
 
     var nearPoint = points[i-5];
 
     if (nearPoint) {
-      line(nearPoint.x, nearPoint.y, points[i].x, points[i].y, 0xFF000000);
+      line(nearPoint.x, nearPoint.y, points[i].x, points[i].y, color);
     }
   }
 
